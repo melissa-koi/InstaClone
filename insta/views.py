bfbf4d5c14d0cf61a6bb8c8ce5de612a841b2406
melissa-koi <melissawangui3@gmail.com>
2021-05-24 00:45:10 +0300

    able to update

--- insta/views.py
+++ insta/views.py
@@ -15,19 +15,19 @@ def profile(request):
     This method handles the user profile
     '''
     title = 'Profile'
+
+    return render(request,'profile.html',{"title":title})
+def update_profile(request):
     if request.method == "POST":
         u_form = UserUpdateForm(request.POST,instance=request.user)
-        p_form = ProfileUpdateForm(request.POST,instance=request.user.profile)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
             messages.success(request,f"You Have Successfully Updated Your Profile!")
             return redirect('profile')
-        else:
-            return redirect('update_profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
-    return render(request,'profile.html',{"title":title})
-def update_profile(request):
-    u_form = UserUpdateForm(instance=request.user)
-    p_form = ProfileUpdateForm(instance=request.user.profile)
     return render(request,'update_profile.html',{"u_form":u_form, "p_form":p_form})
