a6a8d8e43b36ed31ce353715ba4c176bfe9f7262
melissa-koi <melissawangui3@gmail.com>
2021-05-24 21:44:08 +0300

    remove form

--- insta/forms.py
+++ insta/forms.py
@@ -2,15 +2,6 @@ from django import forms
 from .models import Profile, Image, Post
 from django.contrib.auth.models import User
 
-class EditProfileForm(forms.ModelForm):
-    picture = forms.ImageField(required=False)
-    first_name = forms.CharField(widget=forms.TextInput(), max_length=50, required=False)
-    last_name = forms.CharField(widget=forms.TextInput(), max_length=50, required=False)
-    biography = forms.CharField(widget=forms.TextInput(), max_length=260, required=False)
-
-    class Meta:
-        model = Profile
-        fields = ('picture', 'first_name', 'last_name', 'biography')
 
 class UploadImage(forms.ModelForm):
     class Meta:
